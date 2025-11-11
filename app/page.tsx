cd724848e18b503b16520ef55c252fda240015e3
Eva <evatereshenko12321@gmail.com>
2025-11-11 19:30:17 +0200

    CategoriesList

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import GoodInfo from "@/components/common/GoodInfo/GoodInfo";
 import Hero from "@/components/common/Hero/Hero";
-import ReviewsList from "@/components/common/ReviewsList/ReviewsList";
+//import ReviewsList from "@/components/common/ReviewsList/ReviewsList";
 import Style from "@/components/common/Style/Style";
+import CategoriesList from "@/components/common/CategoriesList/CategoriesList";
 // import PopularCategories from "@/components/common/PopularCategories/PopularCategories";
 // import PopularGoods from "@/components/common/PopularGoods/PopularGoods";
 // import ReviewsList from "@/components/common/ReviewsList/ReviewsList";
@@ -11,8 +12,10 @@ export default function Home() {
     <main>
       <Hero />
       <Style />
+      <CategoriesList />
       {/* <PopularCategories /> */}
       <GoodInfo />
+      
       {/* <ReviewsList /> */}
     </main>
   );
