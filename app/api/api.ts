4cf7222012a01559b1c8d65e89545845f7605948
Vitalii Klymenko <Vitalii.Klymenko78@gmail.com>
2025-11-13 16:09:44 +0100

    correct api.ts

--- app/api/api.ts
+++ app/api/api.ts
@@ -5,7 +5,7 @@ import axios, { AxiosError } from 'axios';
 export type ApiError = AxiosError<{ error: string }>;
 
  
-const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_URL; //+ 'api';
+const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_URL + 'api';
 
 export const api = axios.create({
   baseURL: BASE_URL, 
