269b946f0550dd6c32d464174494413dee4f0fc1
Vitalii Klymenko <Vitalii.Klymenko78@gmail.com>
2025-11-13 17:46:00 +0100

    correct api

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import GoodInfo from "@/components/common/GoodInfo/GoodInfo";
 import Hero from "@/components/common/Hero/Hero";
 import PopularCategories from "@/components/common/PopularCategories/PopularCategories";
-import ReviewsList from "@/components/common/ReviewsList/ReviewsList";
+// import ReviewsList from "@/components/common/ReviewsList/ReviewsList";
 import Style from "@/components/common/Style/Style";
-import LastReviews from "@/components/common/LastReviews/LastReviews"
+// import LastReviews from "@/components/common/LastReviews/LastReviews"
 // import PopularCategories from "@/components/common/PopularCategories/PopularCategories";
 // import PopularGoods from "@/components/common/PopularGoods/PopularGoods";
 // import ReviewsList from "@/components/common/ReviewsList/ReviewsList";
