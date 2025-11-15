6b2347bc177ff7231f42cfa6b9d5a047f0994d78
yabluninn <yablunin.artem@gmail.com>
2025-11-15 16:38:32 +0100

    implement goods page

--- app/goods/page.tsx
+++ app/goods/page.tsx
@@ -1,18 +1,34 @@
-'use client';
+"use client";
 
+import { useState } from "react";
 
-import { useState } from 'react';
-import CategoriesFilter from '@/components/common/CategoriesFilter/CategoriesFilter';
-import styles from './GoodsPage.module.css';
+import CategoriesFilter from "@/components/common/CategoriesFilter/CategoriesFilter";
+import GoodsList from "@/components/common/GoodsList/GoodsList";
+
+import styles from "./GoodsPage.module.css";
 
 export default function GoodsPage() {
-  const [activeCategoryName, setActiveCategoryName] = useState<string | null>(null);
+  const [activeCategoryName, setActiveCategoryName] = useState<string | null>(
+    null
+  );
+
+  const title = activeCategoryName ?? "Всі товари";
 
   return (
-    <div className="container">
-      <h2>{activeCategoryName ?? 'Всі товари'}</h2>
-      <CategoriesFilter onCategoryChange={setActiveCategoryName} />
-      {/* <GoodsList/> */}
-    </div>
+    <main className="style">
+      <div className="container">
+        <h1 className="title">{title}</h1>
+
+        <section className={styles.content}>
+          <aside className={styles.sidebar}>
+            <CategoriesFilter onCategoryChange={setActiveCategoryName} />
+          </aside>
+
+          <div className={styles.listArea}>
+            <GoodsList />
+          </div>
+        </section>
+      </div>
+    </main>
   );
 }
