91db6e1fdb8524e60e1246ae53c083a4e816190a
DzyadzoMM <m.dziadzo@gmail.com>
2025-11-15 09:43:42 +0200

    Next server

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -37,7 +37,7 @@ const fetchCurrentUser = async (): Promise<UserProfileData> => {
     }
 }
 
-export const fetchUserOrders = async (): Promise<Order[]> => {
+const fetchUserOrders = async (): Promise<Order[]> => {
     console.log('Fetching user orders (using Next.js API Route)...');
     
     try {
