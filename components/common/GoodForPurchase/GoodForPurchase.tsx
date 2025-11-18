550515785491060a11afab4797ca607a856e2a9d
yelyzaveta057 <lisa.rodionova2002@gmail.com>
2025-11-18 18:41:28 -0500

    remove file

--- components/common/GoodForPurchase/GoodForPurchase.tsx
+++ components/common/GoodForPurchase/GoodForPurchase.tsx
@@ -1,10 +1,8 @@
 import { Good } from "@/types/good";
 import css from "./GoodForPurchase.module.css";
 import Image from "next/image";
-import { useCart } from "@/lib/store/cart";
 
 export default function GoodForPurchase ({good}:{good:Good}) {
-  const addToCart = useCart((s) => s.add);
   
   return (
       <div className="container">
@@ -23,11 +21,7 @@ export default function GoodForPurchase ({good}:{good:Good}) {
                   <h1 className={css.header}>{good.name}</h1>
                   <p className={css.price}> {good.price.value} {good.price.currency} </p>
                   <p className={css.about}>{good.prevDescription }</p>
-                  <button
-        className={css.btn1}
-        onClick={() => {
-          addToCart(good);
-        }}>Додати в кошик</button>
+                  <button className={css.btn1}>Додати в кошик</button>
                   <button className={css.btn2}>Купити зараз</button>
                   <p className={css.text}>Безкоштовна доставка для замовлень від 1000 грн</p>
                   <p>Опис</p>
